9a000c646f891b2c76c98d2c9dc940d6956c6a8d
szymptom <bpodkolzin@kse.org.ua>
2024-11-04 21:25:51 +0200

    collaboration section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -14,8 +14,8 @@ const Benefits = () => {
 
             <div className="flex flex-wrap gap-10 mb-10">
                 {benefits.map((benefit) => (
-                    <div key={benefit.id} className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]
-                    " style={{ backgroundImage: `url(${benefit.backgroundUrl})` }}>
+                    <div key={benefit.id} className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                        style={{ backgroundImage: `url(${benefit.backgroundUrl})` }}>
                         <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none ">
                             <h5 className="h5 mb-5">{benefit.title}</h5>
                             <p className="body-2 mb-6 text-n-3">{benefit.text}</p>
